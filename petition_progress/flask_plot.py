09ca7a57fdac9e95f642e0b60aee42523e71910a
pesh5067@colorado.edu <pesh5067@colorado.edu>
2018-11-14 12:41:37 -0700

    fixing a silly security flaw

--- petition_progress/flask_plot.py
+++ petition_progress/flask_plot.py
@@ -1,5 +1,5 @@
 import io
-from flask import Flask, Response, render_template, send_file
+from flask import Flask, Response, render_template, send_file, request
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -12,10 +12,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def plot_png():
-    fig = plot_make()    
-    output = io.BytesIO()
+    allQ = request.args.get('all')
 
+    if allQ == 'true':
+        fig = plot_make_all()
+
+    else:
+        fig = plot_make_top_ten()    
+
+    output = io.BytesIO()
     output.seek(0)
     plt.savefig(output, format='svg')
     plt.close()
     return Response(output.getvalue(), mimetype='image/svg+xml')
+
--- petition_progress/plot_make.py
+++ petition_progress/plot_make.py
@@ -5,13 +5,14 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from collections import Counter
 import pickle
+import numpy
 
 SCOPE = ['https://spreadsheets.google.com/feeds',
                   'https://www.googleapis.com/auth/drive']
 SECRETS_FILE = "secret_key.json"
 SPREADSHEET = "Graduate workers need a student fee waiver. (Responses)"
 
-def plot_make():
+def plot_make_top_ten():
     # Authenticate using the signed key
     credentials = ServiceAccountCredentials.from_json_keyfile_name(SECRETS_FILE, SCOPE)
     gc = gspread.authorize(credentials)
@@ -29,7 +30,7 @@ def plot_make():
         pickle_save.close()
     
     # Get dept responses
-    depts = [ elem[u'Department or program (4-letter code preferred)'] for elem in responses ]
+    depts = [ elem[u'Department or program (4-letter code preferred)'].strip() for elem in responses ]
     
     # Ditch all responses that aren't 4 letters long (or that aren't unicode strings for some reason)
     # simultaneously convert to a string
@@ -58,4 +59,52 @@ def plot_make():
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
-    return plot, dept_totals
+    return plot
+
+def plot_make_all():
+    # Authenticate using the signed key
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(SECRETS_FILE, SCOPE)
+    gc = gspread.authorize(credentials)
+    
+    # Pull Google Sheet and convert to Pandas DataFrame
+    # get petition responses
+    try:
+        responses = gc.open(SPREADSHEET).sheet1.get_all_records()
+        depts = [ elem[u'Department or program (4-letter code preferred)'].strip() for elem in responses ]
+        
+        pickle_save = open('sheet_save.pickle','wb')
+        pickle.dump(depts, pickle_save)
+        pickle_save.close()
+        
+    except:
+        pickle_save = open('sheet_save.pickle','rb')
+        depts = pickle.load(pickle_save)
+        pickle_save.close()
+        
+    # Ditch all responses that aren't 4 letters long (or that aren't unicode strings for some reason)
+    # simultaneously convert to a string
+    depts = [ str(d).upper() for d in filter( lambda s: (len(s) == 4) & (type(s) == unicode), depts ) ]
+    dept_totals = dict(Counter(depts))
+    dept_totals = sorted([ (val, dept) for dept,val in dept_totals.items() ], reverse=True )
+    dept_totals = [ (dept,val) for val,dept in dept_totals ]
+
+    plot = plt.figure()
+    ax = plot.add_subplot(1,1,1)
+    x_vals = range(len(dept_totals))
+    
+    ax.bar( x_vals,
+              [val for dept,val in dept_totals],
+              align='center',
+              color=(1.,170./255.,60./255.),
+              edgecolor=(1.,170./255.,60./255.)
+    )
+
+    ax.set_ylabel('Signatures')
+    ax.set_xticks( x_vals )
+    ax.set_xticklabels( [dept for dept,val in dept_totals], rotation=90, size=8 )
+    ax.tick_params( direction='in', top=False, right=False)
+    ax.set_xlim(left=-.5, right = max(x_vals) )
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    return plot
